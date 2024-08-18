44ddc51611d858324a1769c0d32a6f789be63fcc
leeseojun <leeseojun34@gmail.com>
2024-08-18 18:55:24 +0900

    fix: db version change

--- glue_privesc/terraform/rds.tf
+++ glue_privesc/terraform/rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "cg-rds" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "13.16"
+  engine_version       = "13.11"
   instance_class       = "db.t3.micro"
   db_subnet_group_name = aws_db_subnet_group.cg-rds-subnet-group.id
   db_name              = var.rds-database-name
